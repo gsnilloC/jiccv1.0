3e88dd35033020dc6cadf8e478e1a8d1b9fe9517
gsnilloC <cgichohi2018@gmail.com>
2025-05-23 20:28:16 -0700

    events backend fully functioning

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,30 @@
 class EventsController < ApplicationController
+  def index
+    @events = Event.all.order(date: :asc) # Fetch all events, ordered by date
+  end
+
+  def new
+    @event = Event.new # Prepare a new event for the form
+  end
+
+  def create
+    @event = Event.new(event_params) # Build a new event from form data
+    if @event.save
+      redirect_to events_path, notice: "Event was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to events_path, notice: "Event was successfully deleted."
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:group, :name, :description, :time, :date)
+  end
 end
